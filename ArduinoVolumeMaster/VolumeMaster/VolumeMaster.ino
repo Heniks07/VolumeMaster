90702c2b1d3fc201d723bb2210bea8877bcae3f9
Heniks07 <hendriksauer2.0@gmail.com>
2024-04-16 16:06:32 +0200

    Switched to continuous Volume updating
    Signed-off-by: Heniks07 <hendriksauer2.0@gmail.com>

--- ArduinoVolumeMaster/VolumeMaster/VolumeMaster.ino
+++ ArduinoVolumeMaster/VolumeMaster/VolumeMaster.ino
@@ -1,5 +1,5 @@
-int pins[2] = { A0, A1};
-int lastVolume[2] = { -10, -10};
+int pins[2] = { A0, A1 };
+int lastVolume[2] = { -10, -10 };
 bool initialized;
 ;
 
@@ -10,41 +10,23 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.println(analogRead(A0));
-  if(Serial.available()){
-    String input = Serial.readStringUntil('\n');
-  
-    if(input.equals("getVolume"))
-      printVolume();
-  }
 
-  if (!initialized)
-    delay(1000);
-
-  bool doPrint = false;
   for (int i = 0; i < (sizeof(pins) / sizeof(int)); i++) {
     int currentVolume = analogRead(pins[i]);
-
-    if (abs(currentVolume - lastVolume[i]) > 5) {
-      lastVolume[i] = currentVolume;
-      doPrint = true;
+    Serial.print(currentVolume);
+    if (i != (sizeof(lastVolume) / sizeof(int)) - 1) {
+      Serial.print("|");
     }
   }
 
-  if (doPrint || !initialized) {
-    printVolume();
-    initialized = true;
-  }
-  delay(12);
-}
+  Serial.println();
 
-void printVolume(){
-  for (int i = 0; i < (sizeof(lastVolume) / sizeof(int)); i++) {
-      Serial.print(lastVolume[i]);
-      if (i != (sizeof(lastVolume) / sizeof(int)) - 1) {
-        Serial.print("|");
-      }
-    }
-    Serial.println();
+  delay(20);
+
+  //for(int i = 0; i != -1; i++){
+  //  Serial.print(i);
+  //  Serial.print("|");
+  //  Serial.print(i);
+  //  Serial.println();
+  //}
 }
