cff28dab46c3bd2e4bb5462499aa77a762c675ec
Heniks07 <hendriksauer2.0@gmail.com>
2024-05-01 18:15:17 +0200

    Arduino reconnection message
    Signed-off-by: Heniks07 <hendriksauer2.0@gmail.com>

--- VolumeMasterCom/GetVolumeHandlers.cs
+++ VolumeMasterCom/GetVolumeHandlers.cs
@@ -18,8 +18,12 @@ public partial class VolumeMasterCom
 
     public (List<int>? SliderIndexesChanged, List<int> Volume) GetVolume()
     {
-        if(_port is {IsOpen: false}) 
+        if (_port is { IsOpen: false })
+        {
             _port.Open();
+            PrintLog("Arduino reconnected", LogLevel.Info);
+        }
+
         try
         {
             var receivedData = _port?.ReadExisting().Split('\n')[^2];
@@ -52,13 +56,13 @@ public partial class VolumeMasterCom
             PrintLog(
                 $"Make sure the right serial port ({Config?.PortName}) is configured in the config file ({ConfigPath()}) and no other application is using the port.",
                 LogLevel.Info);
-        }catch (ArgumentOutOfRangeException e)
+        }
+        catch (ArgumentOutOfRangeException e)
         {
             _port?.Close();
             PrintLog(e.Message, LogLevel.Error);
         }
 
-        
 
         return (_sliderIndexesChanged, _volume);
     }
